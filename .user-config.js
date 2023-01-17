f01995ce26fbf6074990b4cf3b7c4eab24a9a70d
Marcus <47216370+mant-s@users.noreply.github.com>
2023-01-17 15:45:19 +0800

    another edit

--- .user-config.js
+++
@@ -1,7 +0,0 @@
-var userConfigs = {
-    const myApiKey = "1629a413d4909855b4b093edd068a69f-us12";
-    const myAudienceId = "a911a10f9e";
-    const mailchimpServerPrefix = "us12";
-}
-
-module.exports = {userConfigs};
